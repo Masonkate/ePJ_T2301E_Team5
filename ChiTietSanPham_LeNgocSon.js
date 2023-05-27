9de49f9fc3e03b49edc930e3829561be338234b0
Masonkate <Sonlengoc230@gmail.com>
2023-05-27 23:56:34 +0700

    add jpg, jscode

---
+++ ChiTietSanPham_LeNgocSon.js
@@ -0,0 +1,8 @@
+// Lấy tham chiếu đến nút "Thêm vào giỏ hàng"
+var addToCartButton = document.querySelector('.add-to-cart button');
+
+// Đăng ký xử lý sự kiện khi nhấp vào nút "Thêm vào giỏ hàng"
+addToCartButton.addEventListener('click', function() {
+  // Xử lý logic thêm sản phẩm vào giỏ hàng ở đây
+  console.log('Sản phẩm đã được thêm vào giỏ hàng.');
+});
